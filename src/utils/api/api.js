4577e970684406e30507ce9ad51d9d283c60237c
viktoriamay <alterkelly@mail.ru>
2023-05-14 17:42:40 +0300

    Add api requests

---
+++ src/utils/api/api.js
@@ -0,0 +1,52 @@
+const onResponse = (res) => {
+  return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+};
+
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  getProjects() {
+    return fetch(`${this._baseUrl}/projects/`, {
+      method: "GET",
+    }).then(onResponse);
+  }
+
+  getProjectByID(projectID) {
+    return fetch(`${this._baseUrl}/projects/${projectID}/`, {
+      method: "GET",
+    }).then(onResponse);
+  }
+
+  getStudentsInfo() {
+    return fetch(`${this._baseUrl}/student_infos/`, {
+      method: "GET",
+    }).then(onResponse);
+  }
+  getStudentByID(studentID) {
+    return fetch(`${this._baseUrl}/student_infos/${studentID}/`, {
+      method: "GET",
+    }).then(onResponse);
+  }
+
+  createProject(newProject) {
+    return fetch(`/project_create/`, {
+      method: "POST",
+      body: JSON.stringify(newProject),
+      headers: this._headers,
+    }).then(onResponse);
+  }
+}
+
+const config = {
+  baseUrl: "http://84.201.135.211:8002",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+const api = new Api(config);
+
+export default api;
