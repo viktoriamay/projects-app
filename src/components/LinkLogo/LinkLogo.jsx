e167f2bfe1ad2f40a70d715a32370b1b961e731c
viktoriamay <alterkelly@mail.ru>
2023-05-14 18:20:12 +0300

    Edit logo image source and Card style height

--- src/components/LinkLogo/LinkLogo.jsx
+++ src/components/LinkLogo/LinkLogo.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import "./LinkLogo.scss";
+import mainLogo from './img/logo_projects.png'
 
 export const LinkLogo = () => {
   return (
     <Link to="/projects-app">
       <img
         className="link_logo"
-        src="https://upload.wikimedia.org/wikipedia/ru/thumb/8/81/%D0%9B%D0%BE%D0%B3%D0%BE%D1%82%D0%B8%D0%BF_%D0%9D%D0%98%D0%A3_%D0%92%D0%A8%D0%AD.svg/1200px-%D0%9B%D0%BE%D0%B3%D0%BE%D1%82%D0%B8%D0%BF_%D0%9D%D0%98%D0%A3_%D0%92%D0%A8%D0%AD.svg.png"
+        src={mainLogo}
         alt="logo"
       />
     </Link>
