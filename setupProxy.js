7b7b9b86334a127ef31dacf954ba38135ef84577
viktoriamay <alterkelly@mail.ru>
2023-05-16 00:43:54 +0300

    Refactor

--- setupProxy.js
+++ setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/projects/',
     createProxyMiddleware({
-      target: 'http://84.201.135.211:8002/',
+      target: 'http://84.201.135.211:8002',
       changeOrigin: true,
     })
   );
